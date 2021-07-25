988acd12f10a8e4716f61597d104f16f89a1fbcb
SangWon7242 <gotkdqja@gmail.com>
2021-07-25 15:50:42 +0900

    db/schema.sql 파일추가

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+# DB 생성
+DROP DATABASE IF EXISTS sb_c_2021_2nd_t;
+CREATE DATABASE sb_c_2021_2nd_t;
+USE sb_c_2021_2nd_t;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+# 게시물, 테스트 데이터 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목4',
+`body` = '내용4';
