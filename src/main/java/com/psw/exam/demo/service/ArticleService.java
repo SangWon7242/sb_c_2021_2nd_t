813a224f1d47fdf250eed96daffdd4e9338ca219
SangWon7242 <gotkdqja@gmail.com>
2021-08-05 17:33:25 +0900

    C:/Program Files/Git/usr/article/doAdd 에 ResultData 적용

--- src/main/java/com/psw/exam/demo/service/ArticleService.java
+++ src/main/java/com/psw/exam/demo/service/ArticleService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.psw.exam.demo.repository.ArticleRepository;
+import com.psw.exam.demo.util.Ut;
 import com.psw.exam.demo.vo.Article;
+import com.psw.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -19,9 +21,11 @@ public class ArticleService {
 		return articleRepository.getArticles();
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
+		int id = articleRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
 
 	public Article getArticle(int id) {
