1f79444d383d48b8f272d282703fcb092d200e3f
SangWon7242 <gotkdqja@gmail.com>
2021-08-14 16:40:03 +0900

    NeedLogin인터셉터가 여러 액션들을 대신해서 자격없는 요청들을 앞선에서 거름

--- src/main/java/com/psw/exam/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/psw/exam/demo/interceptor/NeedLoginInterceptor.java
@@ -6,11 +6,18 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.psw.exam.demo.vo.Rq;
+
 @Component
 public class NeedLoginInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		System.out.println("=====로그인 필요=====");
+		Rq rq = (Rq) req.getAttribute("rq");
+		
+		if(!rq.isLogined()) {
+			rq.printHisoryBackJs("로그인 후 이용해주세요.");
+			return false;
+		}
 
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
--- src/main/java/com/psw/exam/demo/vo/Rq.java
+++ src/main/java/com/psw/exam/demo/vo/Rq.java
@@ -1,7 +1,12 @@
 package com.psw.exam.demo.vo;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.psw.exam.demo.util.Ut;
 
 import lombok.Getter;
 
@@ -10,8 +15,14 @@ public class Rq {
 	public boolean isLogined;
 	@Getter
 	private int loginedMemberId;
+	
+	private HttpServletRequest req;
+	private HttpServletResponse resp;
 
-	public Rq(HttpServletRequest req) {
+	public Rq(HttpServletRequest req, HttpServletResponse resp) {
+		this.req = req;
+		this.resp = resp;	
+		
 		HttpSession httpSession = req.getSession();
 		boolean isLogined = false;
 		int loginedMemberId = 0; // 로그인 안한 상태를 0으로 둠
@@ -19,12 +30,38 @@ public class Rq {
 		// null이 아니라면 로그인한 상태
 		if (httpSession.getAttribute("loginedMemberId") != null) {
 			isLogined = true;
-			loginedMemberId = (int)httpSession.getAttribute("loginedMemberId");
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
 		}
 		
 		this.isLogined = isLogined;
-		this.loginedMemberId = loginedMemberId;
+		this.loginedMemberId = loginedMemberId;	
+	}
+
+	public void printHisoryBackJs(String msg) {
+		resp.setContentType("text/html; charset=UTF-8");
 		
+		println("<script>");
+		
+		if(!Ut.empty(msg)) {
+			println("alert( '" + msg + "';");
+		}
+		
+		println("history.back();");
+		
+		println("</script>");
+	}
+
+
+	private void print(String str) {
+		try {
+			resp.getWriter().append(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
 	}
 	
+	private void println(String str) {
+		print(str + "\n");
+		
+	}
 }
