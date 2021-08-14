4c58b2963c0ec9126b87b6c6721d1ea55df59cef
SangWon7242 <gotkdqja@gmail.com>
2021-08-14 16:04:35 +0900

    Rq객체를 BeforeAction 인터셉터에서 대표로 한번만 만들기

--- src/main/java/com/psw/exam/demo/MyWebMvcConfigurer.java
+++ src/main/java/com/psw/exam/demo/MyWebMvcConfigurer.java
@@ -6,17 +6,29 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.psw.exam.demo.interceptor.BeforeActionInterceptor;
+import com.psw.exam.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	// beforeActionInterceptor 인터셉터 불러오기
-		@Autowired
-		BeforeActionInterceptor beforeActionInterceptor;
+	@Autowired
+	BeforeActionInterceptor beforeActionInterceptor;
 
-		// 이 함수는 인터셉터를 적용하는 역할을 합니다.
-		@Override
-		public void addInterceptors(InterceptorRegistry registry) {
-			registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
-					.excludePathPatterns("/error");
-		}
+	@Autowired
+	NeedLoginInterceptor needLoginInterceptor;
+
+	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/error");
+
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/**")
+				.excludePathPatterns("/usr/article/write")
+				.excludePathPatterns("/usr/article/doWrite")
+				.excludePathPatterns("/usr/article/modify")
+				.excludePathPatterns("/usr/article/doModify")
+				.excludePathPatterns("/error");
+	}
+
 }
