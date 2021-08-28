b3e1778df7ad4053674314de84ae1f3797481265
SangWon7242 <gotkdqja@gmail.com>
2021-08-28 17:20:55 +0900

    시간출력관련 함수 만들어서 사용

--- src/main/java/com/psw/exam/demo/vo/Article.java
+++ src/main/java/com/psw/exam/demo/vo/Article.java
@@ -16,15 +16,28 @@ public class Article {
 	private String body;
 	private int hitCount;
 	
+	private int extra__sumReactionPoint;
+	private int extra__goodReactionPoint; // 좋아요
+	private int extra__badReactionPoint; // 싫어요
+	
 	private String extra__writerName;
 	private boolean extra__actorCanModify;
 	private boolean extra__actorCanDelete;
 	
-	public String getRegDateForPrint() {
-		return regDate.substring(2, 16);
+	public String getForPrintType1RegDate() {
+		return regDate.substring(2, 16).replace(" ", "<br>");
+	}
+	
+	public String getForPrintType1UpdateDate() {
+		return updateDate.substring(2, 16).replace(" ", "<br>");
 	}
 
-	public String getUpdateDateForPrint() {
-		return updateDate.substring(2, 16);
+	public String getForPrintType2RegDate() {
+		return regDate.substring(2, 16).replace(" ", "<br>");
 	}
+	
+	public String getForPrintType2UpdateDate() {
+		return updateDate.substring(2, 16).replace(" ", "<br>");
+	}
+	
 }
