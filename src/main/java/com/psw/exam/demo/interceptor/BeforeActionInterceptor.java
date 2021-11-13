bd25cd870a47a8dce93ed0c0f9ae0af54cc23bed
SangWon7242 <gotkdqja@gmail.com>
2021-11-13 23:11:44 +0900

    무식하게 처음부터 rq를 무조건 만드는 방식을 수정, 필요할 때만 만듬

--- src/main/java/com/psw/exam/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/psw/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -17,7 +17,8 @@ public class BeforeActionInterceptor implements HandlerInterceptor {
 	
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {	
-		rq.initOnBeforeActionInterceptor();
+		req.setAttribute("rq", rq);
+
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 }
